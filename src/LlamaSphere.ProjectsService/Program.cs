1f2a661127e36d6c85530f7077ed280d4abe4705
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-10-23 15:23:52 +0300

    Restructure project according to new business logic

--- src/LlamaSphere.ProjectsService/Program.cs
+++ src/LlamaSphere.ProjectsService/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,7 +17,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
