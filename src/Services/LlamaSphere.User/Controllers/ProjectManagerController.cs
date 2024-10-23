da39815212b6c39e46abab7919bedf773ed92d82
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-10-23 15:34:17 +0300

    Solution restructuring

---
+++ src/Services/LlamaSphere.User/Controllers/ProjectManagerController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LlamaSphere.User.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectManagerController : ControllerBase
+{
+}
