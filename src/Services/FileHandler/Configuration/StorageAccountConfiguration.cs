c3595af7b774d2ca08b627f74e5ed8d3967f5de1
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-10-25 23:59:19 +0300

    Removed identity service

--- src/Services/FileHandler/Configuration/StorageAccountConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FileHandler.Configuration;
-
-public class StorageAccountConfiguration
-{
-    public string ConnectionString { get; set; }
-    public string ContainerName { get; set; }
-}
