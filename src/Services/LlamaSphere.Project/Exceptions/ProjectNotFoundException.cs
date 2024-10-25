1fc102d3e5ebfba9a898f2ebeedf4447550b93c8
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-10-25 23:48:17 +0300

    Endpoints to upload cvs and jobs

---
+++ src/Services/LlamaSphere.Project/Exceptions/ProjectNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using Core.Exceptions;
+
+namespace LlamaSphere.Project.Exceptions;
+
+public class ProjectNotFoundException : NotFoundException
+{
+    public ProjectNotFoundException(Guid id) : base("Project", id)
+    {
+    }
+}
---
+++ src/Services/LlamaSphere.Project/Features/Projects/GetProjectByIdQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace LlamaSphere.Project.Features.Projects;
+
+public record GetProjectByIdQuery(Guid Id);
+
+public record GetProjectByIdResult(Models.Project Project);
+
+public class GetProjectByIdQueryHandler
+{
+}
